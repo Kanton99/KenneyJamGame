06bec69512b42a47f1f46959b4bb5bc02ee9e978
Kanton99 <avolkov99@gmail.com>
2025-07-19 19:36:21 +0200

    Building levels

--- assets/kenney_tiny_town/Tiled/tiny_town.tsx
+++ assets/kenney_tiny_town/Tiled/tiny_town.tsx
@@ -1,4 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tiny_town" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="../Tilemap/tilemap_packed.png" trans="ff00ff" width="192" height="176"/>
+ <wangsets>
+  <wangset name="Path" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="13" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="14" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="24" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="25" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="26" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="36" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="37" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="38" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="39" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="40" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="41" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="42" wangid="1,1,1,1,1,0,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
